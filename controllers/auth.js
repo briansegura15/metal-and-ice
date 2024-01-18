cfb65318f6651b1986cc759d76a86c84aa70d6cd
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2024-01-18 13:33:38 -0500

    Login, Signup, and Index pages completed for v1 phase

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,9 +14,9 @@ exports.getLogin = (req, res) => {
 exports.postLogin = (req, res, next) => {
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
+    validationErrors.push({msg: "Please enter a valid email address."});
   if (validator.isEmpty(req.body.password))
-    validationErrors.push({ msg: "Password cannot be blank." });
+    validationErrors.push({msg: "Password cannot be blank."});
 
   if (validationErrors.length) {
     req.flash("errors", validationErrors);
@@ -34,11 +34,11 @@ exports.postLogin = (req, res, next) => {
       req.flash("errors", info);
       return res.redirect("/login");
     }
-    req.logIn(user, (err) => {
+    req.logIn(user, err => {
       if (err) {
         return next(err);
       }
-      req.flash("success", { msg: "Success! You are logged in." });
+      req.flash("success", {msg: "Success! You are logged in."});
       res.redirect(req.session.returnTo || "/profile");
     });
   })(req, res, next);
@@ -46,9 +46,9 @@ exports.postLogin = (req, res, next) => {
 
 exports.logout = (req, res) => {
   req.logout(() => {
-    console.log('User has logged out.')
-  })
-  req.session.destroy((err) => {
+    console.log("User has logged out.");
+  });
+  req.session.destroy(err => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
     req.user = null;
@@ -68,13 +68,13 @@ exports.getSignup = (req, res) => {
 exports.postSignup = (req, res, next) => {
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
-    validationErrors.push({ msg: "Please enter a valid email address." });
-  if (!validator.isLength(req.body.password, { min: 8 }))
+    validationErrors.push({msg: "Please enter a valid email address."});
+  if (!validator.isLength(req.body.password, {min: 8}))
     validationErrors.push({
       msg: "Password must be at least 8 characters long",
     });
   if (req.body.password !== req.body.confirmPassword)
-    validationErrors.push({ msg: "Passwords do not match" });
+    validationErrors.push({msg: "Passwords do not match"});
 
   if (validationErrors.length) {
     req.flash("errors", validationErrors);
@@ -91,7 +91,7 @@ exports.postSignup = (req, res, next) => {
   });
 
   User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    {$or: [{email: req.body.email}, {userName: req.body.userName}]},
     (err, existingUser) => {
       if (err) {
         return next(err);
@@ -102,11 +102,11 @@ exports.postSignup = (req, res, next) => {
         });
         return res.redirect("../signup");
       }
-      user.save((err) => {
+      user.save(err => {
         if (err) {
           return next(err);
         }
-        req.logIn(user, (err) => {
+        req.logIn(user, err => {
           if (err) {
             return next(err);
           }
